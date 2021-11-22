56b9a92c3e352f33b229b302916cce8fddf4f246
wavemotion-dave <75039837+wavemotion-dave@users.noreply.github.com>
2021-11-22 06:58:09 -0500

    Fixed version number shown on screen.

--- arm9/source/colecoDS.h
+++ arm9/source/colecoDS.h
@@ -5,7 +5,7 @@
 #include <string.h>
 #include "cpu/z80/drz80/Z80_interface.h"
 
-#define VERSIONCLDS "3.2"
+#define VERSIONCLDS "3.3"
 
 //#define REAL_AY       // Enable this to use the real AY emulation 
 
