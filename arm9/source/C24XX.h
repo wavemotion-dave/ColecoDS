1a9c52aaf9d54ab04f494a4cc56379625d96dd24
Dave Bernazzani <meeplemaster@gmail.com>
2024-04-14 22:13:11 -0400

    9.6e with fix for Activision PCBs with EEPROM so games like Boxxle work again. Improved Cart Selection config.

--- arm9/source/C24XX.h
+++ arm9/source/C24XX.h
@@ -17,16 +17,16 @@ extern "C" {
 #endif
 
 #define C24XX_CHIP    0x0F
-#define C24XX_NONE    0x00 // No EEPROM
-#define C24XX_24C01   0x01 // 128 bytes   (7bit, 8/page)
-#define C24XX_24C02   0x02 // 256 bytes   (8bit, 8/page)
-#define C24XX_24C04   0x03 // 512 bytes   (1+8bit, 16/page)
-#define C24XX_24C08   0x04 // 1024 bytes  (2+8bit, 16/page)
-#define C24XX_24C16   0x05 // 2048 bytes  (3+8bit, 16/page)
-#define C24XX_24C32   0x06 // 4096 bytes  (12bit, 32/page)
-#define C24XX_24C64   0x07 // 8192 bytes  (13bit, 32/page)
-#define C24XX_24C128  0x08 // 16384 bytes (14bit, 64/page)
-#define C24XX_24C256  0x09 // 32768 bytes (15bit, 64/page)
+#define C24XX_24C01   0x00 // 128 bytes   (7bit, 8/page)
+#define C24XX_24C02   0x01 // 256 bytes   (8bit, 8/page)
+#define C24XX_24C04   0x02 // 512 bytes   (1+8bit, 16/page)
+#define C24XX_24C08   0x03 // 1024 bytes  (2+8bit, 16/page)
+#define C24XX_24C16   0x04 // 2048 bytes  (3+8bit, 16/page)
+#define C24XX_24C32   0x05 // 4096 bytes  (12bit, 32/page)
+#define C24XX_24C64   0x06 // 8192 bytes  (13bit, 32/page)
+#define C24XX_24C128  0x07 // 16384 bytes (14bit, 64/page)
+#define C24XX_24C256  0x08 // 32768 bytes (15bit, 64/page)
+#define C24XX_NONE    0x09 // No EEPROM
 #define C24XX_DEBUG   0x10
 
 /* Alternative chip names */
